3245049f45892362e788e924b4079b5c866c93ce
Felix Swinkels <felix@code.nl>
2023-10-03 12:00:15 +0200

    check if snippet already exists

--- src/extension.ts
+++ src/extension.ts
@@ -49,66 +49,81 @@ function createSnippet(
   };
 }
 
+async function createWorkspaceCodeSnippet() {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showErrorMessage("No active text editor found.");
+    return;
+  }
+
+  const selection = editor.document.getText(editor.selection);
+  const name = await vscode.window.showInputBox({
+    prompt: "Enter the name of the snippet",
+  });
+  if (!name) {
+    return;
+  }
+
+  const prefixName = sanitizeName(name);
+  const snippet = createSnippet(
+    name,
+    prefixName,
+    editor.document.languageId,
+    selection
+  );
+
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage("No workspace folder found.");
+    return;
+  }
+
+  const snippetFilePath = path.join(
+    workspaceFolder.uri.fsPath,
+    ".vscode",
+    "workspace.code-snippets"
+  );
+
+  let snippetFileContent: Snippet = {};
+  try {
+    snippetFileContent = await readSnippetFile(snippetFilePath);
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      `Error reading snippet file: ${(error as Error).message}`
+    );
+    return;
+  }
+
+  if (snippetFileContent[name]) {
+    const update = await vscode.window.showQuickPick(
+      ["Change name", "Update snippet"],
+      {
+        placeHolder: `Snippet "${name}" already exists.`,
+      }
+    );
+    if (update === "Change name") {
+      createWorkspaceCodeSnippet();
+      return;
+    }
+  }
+
+  Object.assign(snippetFileContent, snippet);
+
+  try {
+    await writeSnippetFile(snippetFilePath, snippetFileContent);
+    const successMessage = `Snippet "${name}" created successfully. You can now use it by typing "/${prefixName}" in a ${editor.document.languageId} file.`;
+    vscode.window.setStatusBarMessage(successMessage, 30000);
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      `Error creating snippet "${name}": ${(error as Error).message}`
+    );
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "extension.createWorkspaceCodeSnippet",
-    async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        vscode.window.showErrorMessage("No active text editor found.");
-        return;
-      }
-
-      const selection = editor.document.getText(editor.selection);
-      const name = await vscode.window.showInputBox({
-        prompt: "Enter the name of the snippet",
-      });
-      if (!name) {
-        return;
-      }
-
-      const prefixName = sanitizeName(name);
-      const snippet = createSnippet(
-        name,
-        prefixName,
-        editor.document.languageId,
-        selection
-      );
-
-      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-      if (!workspaceFolder) {
-        vscode.window.showErrorMessage("No workspace folder found.");
-        return;
-      }
-
-      const snippetFilePath = path.join(
-        workspaceFolder.uri.fsPath,
-        ".vscode",
-        "workspace.code-snippets"
-      );
-
-      let snippetFileContent: Snippet = {};
-      try {
-        snippetFileContent = await readSnippetFile(snippetFilePath);
-      } catch (error) {
-        vscode.window.showErrorMessage(
-          `Error reading snippet file: ${(error as Error).message}`
-        );
-        return;
-      }
-
-      Object.assign(snippetFileContent, snippet);
-
-      try {
-        await writeSnippetFile(snippetFilePath, snippetFileContent);
-        const successMessage = `Snippet "${name}" created successfully. You can now use it by typing "/${prefixName}" in a ${editor.document.languageId} file.`;
-        vscode.window.setStatusBarMessage(successMessage, 30000);
-      } catch (error) {
-        vscode.window.showErrorMessage(
-          `Error creating snippet "${name}": ${(error as Error).message}`
-        );
-      }
-    }
+    createWorkspaceCodeSnippet
   );
 
   context.subscriptions.push(disposable);
